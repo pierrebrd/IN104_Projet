ef8bb709bec97a2d07423972466fdfc428695528
Juju <julie.aubry26@gmail.com>
2024-04-02 16:17:27 +0200

    02/04 16h16 J'ai esquissé la fonction de déplcament

--- bobail.c
+++ bobail.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+enum DIRECTION = {NORD, NORD-EST, EST, SUD-EST, SUD, SUD-OUEST, OUEST, NORD-OUEST} ;
+
 int ** initialisation() {
     /*crée une matrice 5*5, la remplit et la renvoie
     remplit en place la config initiale de la matrice
@@ -49,8 +51,31 @@ void destruction(int** grille) {
     free(grille) ;
 }
 
+void coup(){
+    /* Demande le coup, vérifie qu'il est légal, et met à jour la grille*/
+
+    // Demande au joueur 
+
+    printf("Emplacement du pion à déplacer (LettreChiffre sans espace): ") ;
+    char* depart ;
+    scanf("%s", depart) ;
+
+    printf("Direction (Nord, Nord-Ouest...): ") ;
+    char* dir_string;
+    scanf("%s", dir_string) ;
+
+    // Vérifie que l'entrée est correcte, convertir dir_string en enum
+    // A FAIRE, ca va etre un gros morceau :(
+
+    //Voir si on peut se déplacer dans cette direction
+
+    // déplacer dans cette direction
+    // on peut définit suivant=i+1,j dans la dir sud, i-1,j+1 dans le dir nord-est...
+}
+
 
 int main() {
-    initialisation()
+    initialisation() ;
+    destruction() ;
 
 }
