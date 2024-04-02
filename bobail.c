9de79d7211a0b132ab5362bea4afc9d909c5857e
pierrebrd <pierre.bordeau.79240@gmail.com>
2024-04-02 16:47:10 +0200

    modifications affichage en prenant en compte les chgts de julie

--- bobail.c
+++ bobail.c
@@ -1,82 +1,9 @@
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <stdio.h>
-
-void afficher()
-{
-    printf("┏━━━┳━━━┳━━━┳━━━┳━━━┓\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n┣━━━╋━━━╋━━━╋━━━╋━━━┫\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n┣━━━╋━━━╋━━━╋━━━╋━━━┫\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n┣━━━╋━━━╋━━━╋━━━╋━━━┫\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n┣━━━╋━━━╋━━━╋━━━╋━━━┫\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n┗━━━┻━━━┻━━━┻━━━┻━━━┛\n");
-}
-
-int main()
-{
-    char *str = "❤️";
-    printf("%s\n", str);
-    printf("┏━━━┳━━━┳━━━┳━━━┳━━━┓\n");
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("┃ ");
-        printf("\033[0;31m");
-        printf("\x1b[1m");
-        printf("● ");
-        printf("\033[0m");
-    }
-    printf("┃\n");
-
-    printf("Hello\n");
-    afficher();
-    return 0;
-}
-=======
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-int ** initialisation() {
+int **initialisation()
+{
     /*crée une matrice 5*5, la remplit et la renvoie
     remplit en place la config initiale de la matrice
 
@@ -90,42 +17,115 @@ int ** initialisation() {
     colonnes j croissant de gauche à droite
     -> le joueur 1 a ses pions dans la colonne j=0, et le joueur 2 dans j=4
     */
-    int **grille = malloc(5 * sizeof(int*));  
-    if (grille == NULL) { // check malloc
-        printf("Echec malloc.") ;
+    int **grille = malloc(5 * sizeof(int *));
+    if (grille == NULL)
+    { // check malloc
+        printf("Echec malloc.");
     }
-	for(int k = 0; k < 5; k++) {
-        grille[k] = malloc(5* sizeof(int));
-        if (grille[k] == NULL) { // check malloc
-        printf("Echec malloc.") ;
+    for (int k = 0; k < 5; k++)
+    {
+        grille[k] = malloc(5 * sizeof(int));
+        if (grille[k] == NULL)
+        { // check malloc
+            printf("Echec malloc.");
         }
     }
-		
 
-    for (int i = 0 ; i <= 4 ; i++) {
-        grille[i][0] = 1 ;
-        grille[i][4] = 2 ;
-        for (int j = 1 ; j <= 3 ; j++) {
-            grille [i][j] = 0 ;
-            }
+    for (int i = 0; i <= 4; i++)
+    {
+        grille[i][0] = 1;
+        grille[i][4] = 2;
+        for (int j = 1; j <= 3; j++)
+        {
+            grille[i][j] = 0;
         }
-    grille[3][3] = 3;
+    }
+    grille[2][2] = 3;
 
-    return grille ;
+    return grille;
 }
 
-void destruction(int** grille) {
+void destruction(int **grille)
+{
     /*detruit la grille en libérant l'espace mémoire*/
 
-    for(int k = 0; k < 5; k++) {
-        free(grille[k]) ;
-        }
-    free(grille) ;
+    for (int k = 0; k < 5; k++)
+    {
+        free(grille[k]);
+    }
+    free(grille);
 }
 
-
-int main() {
-    initialisation()
-
+void affichercase(int i, int j, int **grille)
+{
+    if (grille[i][j] == 1)
+    {
+        printf("\033[0;31m");
+        printf("● ");
+        printf("\033[0m");
+    }
+    else if (grille[i][j] == 2)
+    {
+        printf("\033[0;32m");
+        printf("\033[1m");
+        printf("◯ ");
+        printf("\033[0m");
+    }
+    else if (grille[i][j] == 3)
+    {
+        printf("\033[0;35m");
+        printf("➊ ");
+        printf("\033[0m");
+    }
+    else
+    {
+        printf("  ");
+    }
 }
->>>>>>> e7f6c4f478a10d9dffe3fe6d0637116c030bbcfb
+
+void afficher(int **grille)
+{
+    printf("\n    1   2   3   4   5\n  ┏━━━┳━━━┳━━━┳━━━┳━━━┓\nA ");
+    for (int j = 0; j < 5; ++j)
+    {
+        printf("┃ ");
+        affichercase(0, j, grille);
+        printf("\033[0m");
+    }
+    printf("┃\n  ┣━━━╋━━━╋━━━╋━━━╋━━━┫\nB ");
+    for (int j = 0; j < 5; ++j)
+    {
+        printf("┃ ");
+        affichercase(1, j, grille);
+        printf("\033[0m");
+    }
+    printf("┃\n  ┣━━━╋━━━╋━━━╋━━━╋━━━┫\nC ");
+    for (int j = 0; j < 5; ++j)
+    {
+        printf("┃ ");
+        affichercase(2, j, grille);
+        printf("\033[0m");
+    }
+    printf("┃\n  ┣━━━╋━━━╋━━━╋━━━╋━━━┫\nD ");
+    for (int j = 0; j < 5; ++j)
+    {
+        printf("┃ ");
+        affichercase(3, j, grille);
+        printf("\033[0m");
+    }
+    printf("┃\n  ┣━━━╋━━━╋━━━╋━━━╋━━━┫\nE ");
+    for (int j = 0; j < 5; ++j)
+    {
+        printf("┃ ");
+        affichercase(4, j, grille);
+        printf("\033[0m");
+    }
+    printf("┃\n  ┗━━━┻━━━┻━━━┻━━━┻━━━┛\n");
+}
+
+int main()
+{
+    int **grille = initialisation();
+    afficher(grille);
+    return 0;
+}
