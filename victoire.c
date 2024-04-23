ddcd59f641d0023f722553bb3223bc28e652b9b1
pierrebrd <pierre.bordeau.79240@gmail.com>
2024-04-23 17:28:34 +0200

    23/04 17h27 réparation de victoire.c

--- victoire.c
+++ victoire.c
@@ -35,41 +35,47 @@ int victoire(int **grille, int joueur)
         return 2;
     }
 
+    // Cas ou le bobail est dans la 1er ou la denriere colonne
+    if (y == 0)
+    {
+        return 1;
+    }
+    else if (y == 4)
+    {
+        return 2;
+    }
+
     // Cas ou le bobail ne peut pas bouger
     int voisins_occupés = 0;
-    if (legit(grille, x, y, x, y + 1, 3) != 0)
+    if (y + 1 > 4 || legit(grille, x, y, x, y + 1, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x, y, 3) != 0)
+    if (y - 1 < 0 || legit(grille, x, y, x, y - 1, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x, y - 1, 3) != 0)
+    if (x + 1 > 4 || y + 1 > 4 || legit(grille, x, y, x + 1, y + 1, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x - 1, y + 1, 3) != 0)
+    if (x + 1 > 4 || legit(grille, x, y, x + 1, y, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x - 1, y, 3) != 0)
+    if (x + 1 > 4 || y - 1 < 0 || legit(grille, x, y, x + 1, y - 1, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x - 1, y - 1, 3) != 0)
+    if (x - 1 < 0 || y + 1 > 4 || legit(grille, x, y, x - 1, y + 1, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x + 1, y + 1, 3) != 0)
+    if (x - 1 < 0 || legit(grille, x, y, x - 1, y, 3) != 0)
     {
         voisins_occupés++;
     }
-    if (legit(grille, x, y, x + 1, y, 3) != 0)
-    {
-        voisins_occupés++;
-    }
-    if (legit(grille, x, y, x - 1, y - 1, 3) != 0)
+    if (x - 1 < 0 || y - 1 < 0 || legit(grille, x, y, x - 1, y - 1, 3) != 0)
     {
         voisins_occupés++;
     }
