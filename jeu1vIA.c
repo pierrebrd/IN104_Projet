acf908da12ef641ae9acafb32be5503ec495cfed
pierrebrd <pierre.bordeau.79240@gmail.com>
2024-04-30 15:07:10 +0200

    30/04 15h06 remplacement de "grille" par "jeu"

--- jeu1vIA.c
+++ jeu1vIA.c
@@ -8,61 +8,65 @@
 #include "legit.h"
 #include "initialisation.h"
 #include "victoire.h"
+#include "hasard.h"
 
 void jeu1vIA()
 {
     // Demander au joueur s'il est joueur 1 ou joueur 2
-    int joueurHumain ;
-    int joueurIA ;
-    printf("Voulez vous etre le joueur : 1 ou 2? : ") ;
-    scanf("%d",&joueurHumain) ;
-    printf("\n") ;
-    joueurIA = joueurHumain %2 + 1 ;
+    int joueurHumain;
+    int joueurIA;
+    printf("Voulez vous être le joueur : 1 ou 2 ? : ");
+    scanf("%d", &joueurHumain);
+    printf("\n");
+    joueurIA = joueurHumain % 2 + 1;
 
     // expliquer l'affichage
-    if (joueurHumain == 1 ) {
-        printf("Vous avez les pions rouges, l'IA a les pions verts. Le bobail est en violet\n") ;
+    if (joueurHumain == 1)
+    {
+        printf("Vous avez les pions rouges, l'IA a les pions verts. Le bobail est en violet\n");
     }
-    else {
-       printf("Vous avez les pions verts, l'IA a les pions rouges. Le bobail est en violet\n") ;
+    else
+    {
+        printf("Vous avez les pions verts, l'IA a les pions rouges. Le bobail est en violet\n");
     }
 
-
-    int **grille = initialisation();
+    jeu_t *jeu = initialisation();
     int tour = 0;
     int joueuractuel = 1;
-    while (victoire(grille) == 0) // tant qu'aucun joueur n'a gagné
+    while (victoire(jeu) == 0) // tant qu'aucun joueur n'a gagné
     {
         ++tour;
         if (tour != 1) // au premier tour, on ne bouge pas le bobail
         {
-            if(joueurHumain == joueuractuel) {
-                afficher(grille);
+            if (joueurHumain == joueuractuel)
+            {
+                afficher(jeu);
                 printf("A vous de jouer ! Bougez le Bobail.\n", joueuractuel);
-                coup(grille, 3);
+                coup(jeu, 3);
             }
-            else {
+            else
+            {
                 // COUP DE L'IA
-                afficher(grille);
+                afficher(jeu);
             }
-            
         }
-        if (victoire(grille) == 0)
+        if (victoire(jeu) == 0)
         {
-            if(joueurHumain == joueuractuel) {
-                afficher(grille);
+            if (joueurHumain == joueuractuel)
+            {
+                afficher(jeu);
                 printf("Bougez un de vos pions.\n", joueuractuel);
-                coup(grille, joueuractuel);
+                coup(jeu, joueuractuel);
             }
-            else {
+            else
+            {
                 // COUP DE L'IA
-                afficher(grille);
+                afficher(jeu);
             }
-
         }
 
         joueuractuel = 1 + joueuractuel % 2;
     }
-    afficher(grille);
-    printf("Le joueur %d a gagné en %d tours !\n\n\n", victoire(grille), tour);
+    afficher(jeu);
+    printf("Le joueur %d a gagné en %d tours !\n\n\n", victoire(jeu), tour);
 }
--- jeuIAvIA.c
+++ jeuIAvIA.c
@@ -12,25 +12,25 @@
 
 void jeuIAvIA()
 {
-    int **grille = initialisation();
+    jeu_t *jeu = initialisation();
     int tour = 0;
     int joueuractuel = 1;
-    while (victoire(grille) == 0) // tant qu'aucun joueur n'a gagné
+    while (victoire(jeu) == 0) // tant qu'aucun joueur n'a gagné
     {
         ++tour;
         if (tour != 1) // au premier tour, on ne bouge pas le bobail
         {
             // coup ia
-            afficher(grille);
+            afficher(jeu);
         }
-        if (victoire(grille) == 0)
+        if (victoire(jeu) == 0)
         {
             // coup ia
-            afficher(grille);
+            afficher(jeu);
         }
 
         joueuractuel = 1 + joueuractuel % 2;
     }
-    afficher(grille);
-    printf("Le joueur %d a gagné en %d tours !\n\n\n", victoire(grille), tour);
+    afficher(jeu);
+    printf("Le joueur %d a gagné en %d tours !\n\n\n", victoire(jeu), tour);
 }
