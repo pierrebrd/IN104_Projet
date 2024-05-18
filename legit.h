78a23ae141707c2b6eeb9e0c0ca6a4ef77156e63
Juju <julie.aubry26@gmail.com>
2024-05-18 14:35:23 +0200

    18/05 14h35 je push le début de legit_direction

--- legit.h
+++ legit.h
@@ -11,3 +11,12 @@ du type du pion
 
 renvoie 0 si déplacement correct, ou des codes d'erreur de 1 à 6 en fonction du problème si le déplacement est impossible
 */
+
+int legit_direction(jeu_t *jeu, int indice_coup) :
+/*
+Détermine s'il est possible de jouer le coup, ie si le bobail peut se dépacer selon direction_bobail et si le pion peut se déplacer selon direction_pion.
+
+ARGUMENTS
+jeu_t* jeu : contient une matrice int** correspondant à l'occupation de chaque case du plateau de jeu (0 si vide, 1 si pion du joueur 1, 2 si pion du joueur 2, 3 si bobail) ; une liste x_pions contenant les absisses des positions des pions ; une liste int* y_pions contenant les ordonnées des positions des pions
+
+*/
